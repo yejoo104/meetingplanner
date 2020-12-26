5473da1185f62f8291e348c34398b35615b5f2db
한예주 <yejoo_han@college.harvard.edu>
2020-12-26 21:20:53 +0900

    created new route with meeting_id

--- application.py
+++ application.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def plannerhome():
     return render_template("home.html")
+
+@app.route("/<meeting_id>")
+def get_meeting(meeting_id):
+    return str(meeting_id)
