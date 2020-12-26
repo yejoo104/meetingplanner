341331e2dde6081cff674a128ad5d989bf30c797
한예주 <yejoo_han@college.harvard.edu>
2020-12-27 02:07:57 +0900

    added password hash and debugged time slot issues

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect
 import sqlite3
 import uuid
+
+from flask import Flask, render_template, request, redirect
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 
@@ -10,11 +12,12 @@ def plannerhome():
         return render_template("home.html")
     if request.method == "POST":
         formdata = request.form
+        passwordhash = generate_password_hash(formdata["password"])
         code = str(uuid.uuid4())
         with sqlite3.connect("database.db") as connection:
             cursor = connection.cursor()
             command = "INSERT INTO MEETING (eventname, email, name, password, min_event, max_event, min_people, max_people, dates, start_time, end_time, code) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-            cursor.execute(command, (formdata["eventname"], formdata["email"], formdata["name"], formdata["password"], formdata["min_event"], formdata["max_event"], formdata["min_people"], formdata["max_people"], formdata["dates"], formdata["start_time"], formdata["end_time"], code))
+            cursor.execute(command, (formdata["eventname"], formdata["email"], formdata["name"], passwordhash, formdata["min_event"], formdata["max_event"], formdata["min_people"], formdata["max_people"], formdata["dates"], formdata["start_time"], formdata["end_time"], code))
         
             connection.commit()
             connection.close
