01ae88232d7571023d3ae1e1d487c1145302668e
한예주 <yejoo_han@college.harvard.edu>
2020-12-26 22:37:35 +0900

    can enter form info into db

--- application.py
+++ application.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import sqlite3
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def plannerhome():
-    return render_template("home.html")
+    if request.method == "GET":
+        return render_template("home.html")
+    if request.method == "POST":
+        formdata = request.form
+        with sqlite3.connect("database.db") as connection:
+            cursor = connection.cursor()
+            command = "INSERT INTO MEETING (eventname, email, name, password, min_event, max_event, min_people, max_people, dates, start_time, end_time, code) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+            cursor.execute(command, (formdata["eventname"], formdata["email"], formdata["name"], formdata["password"], formdata["min_event"], formdata["max_event"], formdata["min_people"], formdata["max_people"], formdata["dates"], formdata["start_time"], formdata["end_time"], "fddjkf"))
+        
+            connection.commit()
+            connection.close
+        return redirect("/")
 
 @app.route("/<meeting_id>")
 def get_meeting(meeting_id):
