a82bc3c685109514ac42ccc2d1a24bf43cd19d3c
한예주 <yejoo_han@college.harvard.edu>
2020-12-27 01:49:56 +0900

    creates meeting code

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import sqlite3
+import uuid
 
 app = Flask(__name__)
 
@@ -9,14 +10,15 @@ def plannerhome():
         return render_template("home.html")
     if request.method == "POST":
         formdata = request.form
+        code = str(uuid.uuid4())
         with sqlite3.connect("database.db") as connection:
             cursor = connection.cursor()
             command = "INSERT INTO MEETING (eventname, email, name, password, min_event, max_event, min_people, max_people, dates, start_time, end_time, code) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-            cursor.execute(command, (formdata["eventname"], formdata["email"], formdata["name"], formdata["password"], formdata["min_event"], formdata["max_event"], formdata["min_people"], formdata["max_people"], formdata["dates"], formdata["start_time"], formdata["end_time"], "fddjkf"))
+            cursor.execute(command, (formdata["eventname"], formdata["email"], formdata["name"], formdata["password"], formdata["min_event"], formdata["max_event"], formdata["min_people"], formdata["max_people"], formdata["dates"], formdata["start_time"], formdata["end_time"], code))
         
             connection.commit()
             connection.close
-        return redirect("/")
+        return redirect("/" + code)
 
 @app.route("/<meeting_id>")
 def get_meeting(meeting_id):
